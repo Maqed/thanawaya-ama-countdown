0454a4cf9fc6733765a3306c715713461b9214e9
Maged Ibrahim <67274056+Maqed@users.noreply.github.com>
2024-07-09 16:03:39 +0300

    Change the time to 11:00PM GMT+3

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,7 +6,7 @@ function Hero() {
     <div className="flex justify-around items-center flex-col gap-5 py-10 min-h-[calc(100vh-120px)]">
       <Sho3ba
         name="علمي رياضة"
-        finishDate={new Date(`${CURRENT_YEAR}-07-20T09:00:00Z`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-20T08:00:00Z`)}
       />
       <Sho3ba
         name="علمي علوم"
