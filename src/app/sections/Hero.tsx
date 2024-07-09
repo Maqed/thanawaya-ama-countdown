7fc55d1732e6581499593fe51f6d1f6eb287e9bb
Maged Ibrahim <magedibra@yahoo.com>
2024-07-09 13:19:50 +0300

    Fix UI bug

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,7 +1,7 @@
 import Sho3ba from "@/components/sho3ba";
 function Hero() {
   return (
-    <div className="flex justify-around items-center flex-col gap-5 py-10 min-h-[calc(100vh-112px)]">
+    <div className="flex justify-around items-center flex-col gap-5 py-10 min-h-[calc(100vh-120px)]">
       <Sho3ba name="علمي رياضة" finishDate={new Date("2024-07-20T09:00:00Z")} />
       <Sho3ba name="علمي علوم" finishDate={new Date("2024-07-17T09:00:00Z")} />
       <Sho3ba name="ادبي" finishDate={new Date("2024-07-17T09:00:00Z")} />
