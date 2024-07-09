63714659c01230000caa11269649d8bbc8773f1e
Maged Ibrahim <magedibra@yahoo.com>
2024-07-09 13:33:46 +0300

    Improved UI

--- src/components/sho3ba.tsx
+++ src/components/sho3ba.tsx
@@ -1,5 +1,15 @@
 "use client";
 import Countdown from "react-countdown";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 type Sho3baProps = {
   name: string;
@@ -10,14 +20,60 @@ function Sho3ba({ name, finishDate }: Sho3baProps) {
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-2xl font-bold">{name}</h1>
-      <Countdown className="text-3xl" date={finishDate}>
+      <Sho3baCountdown name={name} finishDate={finishDate} />
+    </div>
+  );
+}
+function Sho3baCountdown({ name, finishDate }: Sho3baProps) {
+  const renderer = ({
+    days,
+    hours,
+    minutes,
+    seconds,
+    completed,
+  }: {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+    completed: boolean;
+  }) => {
+    if (completed) {
+      // Render a completed state
+      return (
         <h3 className="text-3xl">
           الف الف مبروك لشعبة{" "}
           <span className="text-primary font-bold">{name}</span> انهم خلصوا
           الامتحانات
         </h3>
-      </Countdown>
-    </div>
+      );
+    } else {
+      // Render a countdown
+      return (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>الايام</TableHead>
+              <TableHead>الساعات</TableHead>
+              <TableHead>الدقايق</TableHead>
+              <TableHead>الثواني</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell>{days}</TableCell>
+              <TableCell>{hours}</TableCell>
+              <TableCell>{minutes}</TableCell>
+              <TableCell>{seconds}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      );
+    }
+  };
+  return (
+    <Countdown className="text-3xl" renderer={renderer} date={finishDate} />
   );
 }
+
 export default Sho3ba;
