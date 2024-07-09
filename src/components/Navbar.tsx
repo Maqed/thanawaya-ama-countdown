c5a933999f3da2a608fb50e1b974ec05b569afe3
Maged Ibrahim <magedibra@yahoo.com>
2024-07-09 13:42:50 +0300

    Add current year so the website works on all years

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { APP_NAME, GITHUB_REPO } from "@/consts/siteData";
+import { APP_NAME, CURRENT_YEAR, GITHUB_REPO } from "@/consts/siteData";
 import { ThemeToggle } from "./ui/theme-toggle";
 import { Github } from "lucide-react";
 import Link from "next/link";
@@ -6,7 +6,9 @@ import Link from "next/link";
 function Navbar() {
   return (
     <nav className="px-2 py-3 flex flex-row items-center justify-between">
-      <h1 className="text-2xl text-primary font-semibold">{APP_NAME}</h1>
+      <h1 className="text-2xl text-primary font-semibold">
+        {APP_NAME} {CURRENT_YEAR}
+      </h1>
       <div className="flex justify-between gap-3 items-center">
         <Link target="_blank" href={GITHUB_REPO}>
           <Github />
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,21 @@
 import Sho3ba from "@/components/sho3ba";
+import { CURRENT_YEAR } from "@/consts/siteData";
+
 function Hero() {
   return (
     <div className="flex justify-around items-center flex-col gap-5 py-10 min-h-[calc(100vh-120px)]">
-      <Sho3ba name="علمي رياضة" finishDate={new Date("2024-07-20T09:00:00Z")} />
-      <Sho3ba name="علمي علوم" finishDate={new Date("2024-07-17T09:00:00Z")} />
-      <Sho3ba name="ادبي" finishDate={new Date("2024-07-17T09:00:00Z")} />
+      <Sho3ba
+        name="علمي رياضة"
+        finishDate={new Date(`${CURRENT_YEAR}-07-20T09:00:00Z`)}
+      />
+      <Sho3ba
+        name="علمي علوم"
+        finishDate={new Date(`${CURRENT_YEAR}-07-17T09:00:00Z`)}
+      />
+      <Sho3ba
+        name="ادبي"
+        finishDate={new Date(`${CURRENT_YEAR}-07-17T09:00:00Z`)}
+      />
     </div>
   );
 }
