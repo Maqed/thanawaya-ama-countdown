cad950be59d1f534a0736f337bf5b5e2f91fa2fb
Maged Ibrahim <magedibra@yahoo.com>
2024-07-17 22:55:01 +0300

    Add graduation cap to the emojis

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -6,17 +6,17 @@ function Hero() {
     <div className="flex justify-around items-center flex-col gap-5 py-10 min-h-[calc(100vh-64px-58px)]">
       <Sho3ba
         name="علمي رياضة"
-        finishEmojis={["📐", "✖️", "♾️", "𝞹", "🧠", "e"]}
+        finishEmojis={["📐", "✖️", "♾️", "𝞹", "🧠", "e", "🎓"]}
         finishDate={new Date(`${CURRENT_YEAR}-07-20T08:00:00Z`)}
       />
       <Sho3ba
         name="علمي علوم"
-        finishEmojis={["🧪", "🧬", "🌱", "⌬", "🌏", "👨🏻‍🔬"]}
+        finishEmojis={["🧪", "🧬", "🌱", "⌬", "🌏", "👨🏻‍🔬", "🎓"]}
         finishDate={new Date(`${CURRENT_YEAR}-07-17T09:00:00Z`)}
       />
       <Sho3ba
         name="ادبي"
-        finishEmojis={["📜", "🎭", "🎨", "🏛️", "📖"]}
+        finishEmojis={["📜", "🎭", "🎨", "🏛️", "📖", "🎓"]}
         finishDate={new Date(`${CURRENT_YEAR}-07-17T09:00:00Z`)}
       />
     </div>
