2e5bbc0969981d115808e2c2faa55c98eaf9fe4b
Maqed <magedibra@yahoo.com>
2025-03-11 15:12:58 +0200

    feat: add 2025 finish dates

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,17 +13,17 @@ function Hero() {
       <Sho3ba
         name="علمي رياضة"
         finishEmojis={["📐", "✖️", "♾️", "𝞹", "🧠", "e", "🎓"]}
-        finishDate={new Date(`${CURRENT_YEAR}-07-20T08:00:00Z`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-10T08:00:00Z`)}
       />
       <Sho3ba
         name="علمي علوم"
         finishEmojis={["🧪", "🧬", "🌱", "⌬", "🌏", "👨🏻‍🔬", "🎓"]}
-        finishDate={new Date(`${CURRENT_YEAR}-07-17T09:00:00Z`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-10T09:00:00Z`)}
       />
       <Sho3ba
         name="ادبي"
         finishEmojis={["📜", "🎭", "🎨", "🏛️", "📖", "🎓"]}
-        finishDate={new Date(`${CURRENT_YEAR}-07-17T09:00:00Z`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-10T09:00:00Z`)}
       />
     </div>
   );
