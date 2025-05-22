4580940fe7d60cec8614871136c865857b6c7700
Maqed <magedibra@yahoo.com>
2025-05-22 17:09:52 +0300

    chore: change portfolio URL

--- src/consts/siteData.ts
+++ src/consts/siteData.ts
@@ -2,7 +2,7 @@ export const APP_NAME = "Thanawaya Ama Countdown";
 export const APP_LOGO = "/logo.png";
 
 // Creator
-export const CREATOR_LINK = "https://magedibrahim.vercel.app/";
+export const CREATOR_LINK = "https://magedibrahim.is-a.dev/";
 export const CREATOR = "Maged Ibrahim";
 export const CREATOR_IMG_SRC = "/Maged Ibrahim.png";
 // Twitter
