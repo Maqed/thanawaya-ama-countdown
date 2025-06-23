69cea42c86b0bbbeb376807e5350727dfd505c73
Maqed <magedibra@yahoo.com>
2025-06-23 12:02:28 +0300

    fix: timezone

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -12,24 +12,24 @@ function Hero() {
     <div className="flex justify-center items-center flex-col gap-5">
       <Sho3ba
         name="علمي رياضة"
-        startDate={new Date(`${CURRENT_YEAR}-06-15T09:00:00+02:00`)}
+        startDate={new Date(`${CURRENT_YEAR}-06-15T09:00:00+03:00`)}
         startEmojis={["💪", "🔥", "⚡", "🚀", "💯", "🎯", "💫"]}
         finishEmojis={["📐", "✖️", "♾️", "𝞹", "🧠", "e", "🎓"]}
-        finishDate={new Date(`${CURRENT_YEAR}-07-10T11:00:00+02:00`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-10T11:00:00+03:00`)}
       />
       <Sho3ba
         name="علمي علوم"
-        startDate={new Date(`${CURRENT_YEAR}-06-15T09:00:00+02:00`)}
+        startDate={new Date(`${CURRENT_YEAR}-06-15T09:00:00+03:00`)}
         startEmojis={["💪", "🔥", "⚡", "🚀", "💯", "🎯", "💫"]}
         finishEmojis={["🧪", "🧬", "🌱", "⌬", "🌏", "👨🏻‍🔬", "🎓"]}
-        finishDate={new Date(`${CURRENT_YEAR}-07-10T12:00:00+02:00`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-10T12:00:00+03:00`)}
       />
       <Sho3ba
         name="ادبي"
-        startDate={new Date(`${CURRENT_YEAR}-06-15T09:00:00+02:00`)}
+        startDate={new Date(`${CURRENT_YEAR}-06-15T09:00:00+03:00`)}
         startEmojis={["💪", "🔥", "⚡", "🚀", "💯", "🎯", "💫"]}
         finishEmojis={["📜", "🎭", "🎨", "🏛️", "📖", "🎓"]}
-        finishDate={new Date(`${CURRENT_YEAR}-07-10T12:00:00+02:00`)}
+        finishDate={new Date(`${CURRENT_YEAR}-07-10T12:00:00+03:00`)}
       />
     </div>
   );
